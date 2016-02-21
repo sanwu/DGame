ee3322785d193bd94b6187727c21583fe297a2db
Sanwu-three <xtygah14three@hotmail.com>
2016-02-21 20:33:01 +0800

    初始化

---
+++ Source/DGame/DGame.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DGame.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DGame, "DGame" );
---
+++ Source/DGame/DGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DGAME_API ADGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
